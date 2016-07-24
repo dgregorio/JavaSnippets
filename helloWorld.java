c328fd5d1310e0816cf22eada87fae093db8a9ea
dgregorio <dgregorio.libunao@gmail.com>
2016-07-24 18:09:01 +0800

    Added code
    Added it works line of code to print.

--- helloWorld.java
+++ helloWorld.java
@@ -1,7 +1,8 @@
 class helloWorld {
   public static void main(String[] args) {
     System.out.println("Hello World!");
-    System.out.println("Testing Git!")
+    System.out.println("Testing Git!");
+    System.out.println("It works!");
   }
 
 }
